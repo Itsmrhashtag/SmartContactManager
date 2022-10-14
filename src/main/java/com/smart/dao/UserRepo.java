d281a0c1fec338c3728118ee02ea79feeb05a490
aayushrai05 <aayush05rai@gmail.com>
2022-10-14 07:21:16 +0530

    updated file

--- src/main/java/com/smart/dao/UserRepo.java
+++ src/main/java/com/smart/dao/UserRepo.java
@@ -8,6 +8,6 @@ import com.smart.entities.User;
 
 public interface UserRepo extends JpaRepository<User,Integer>{
 	
-	@Query("select u from User u where u.email = :email")
+	@Query("select u from User u where u.email =:email")
 	public User getUserByUserName(@Param("email") String email);
 }
