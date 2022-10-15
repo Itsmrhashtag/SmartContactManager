be2176353b5d8d65cea11eca30812af9b4b1f46d
Pushpraj Kushwaha <kpushpraj54@gmail.com>
2022-10-16 00:10:25 +0530

    dashboard

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -5,9 +5,12 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.dao.UserRepo;
+import com.smart.entities.Contact;
 import com.smart.entities.User;
 
 @Controller
@@ -17,8 +20,9 @@ public class UserController {
 	@Autowired
 	private UserRepo userRepo;
 	
-	@RequestMapping("/index")
-	public String dashboard(Model model, Principal principal) {
+	@ModelAttribute
+	public void addCommonData(Model model,Principal principal) {
+		
 		String userName =principal.getName();
 		System.out.println("USERNAME "+userName);
 		
@@ -27,7 +31,21 @@ public class UserController {
 		
 		model.addAttribute("user", user);
 		
+	}
+	
+	@RequestMapping("/index")
+	public String dashboard(Model model, Principal principal) {
+	
 		//get the user using username(email)
 		return "normal/user_dashboard";
+	}
+	
+	//open add form handler
+	@GetMapping("/add-contact")
+	public String openAddContactForm(Model model) {
+		
+		model.addAttribute("title", "Add Contact");
+		model.addAttribute("contact", new Contact());
+		return "normal/add_contact_form";
 	}
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,13 @@
 console.log("this is script")
+
+const toggleSlidebar=()=>{
+
+if($('.sidebar').is(":visible")){
+
+    $(".sidebar").css("display","none");
+    $(".content").css("margin-left","0%");
+}else{
+    $(".sidebar").css("display","block");
+    $(".content").css("margin-left","20%");
+}
+};
